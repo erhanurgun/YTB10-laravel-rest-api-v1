ed9e6456c1006cbb4a81a272a10df2c4dee50dbf
PCI-ACER\erhan <erhan.rgn04@gmail.com>
2022-11-27 20:12:30 +0300

    veri parcalama islemi tamamlandi

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,18 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
+use App\Repositories\ProductRepository;
+use Illuminate\Http\JsonResponse;
+use App\Traits\ResponseTrait;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+    use ResponseTrait;
+
+    private $productRepository;
+
+    public function __construct(ProductRepository $productRepository)
+    {
+        $this->productRepository = $productRepository;
+    }
+
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(): JsonResponse
     {
-        //
+        try {
+            return $this->responseSuccess($this->productRepository->getAll(), 'Ürünler başarıyla listelendi.');
+        } catch (Exception $e) {
+            return $this->responseError([], $e->getMessage());
+        }
     }
 
     /**
@@ -29,7 +46,7 @@ class ProductController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -40,18 +57,22 @@ class ProductController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show($id): JsonResponse
     {
-        //
+        try {
+            return $this->responseSuccess([], 'Ürün başarıyla listelendi.');
+        } catch (Exception $e) {
+            return $this->responseError([], $e->getMessage());
+        }
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
@@ -62,8 +83,8 @@ class ProductController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
@@ -74,7 +95,7 @@ class ProductController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
